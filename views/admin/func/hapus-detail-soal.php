b391cf2b5f68ed5a4a679be623ce137e1d636027
AryaRyuga07 <aryaryuga3@gmail.com>
2023-05-01 16:46:27 +0700

    initial commit

---
+++ views/admin/func/hapus-detail-soal.php
@@ -0,0 +1,26 @@
+<?php 
+require '../../../model/db.php';
+error_reporting(0);
+
+function hapus($id)
+{
+	global $conn;
+	mysqli_query($conn, "DELETE FROM tb_detail_soal WHERE id_soal = $id");
+	return mysqli_affected_rows($conn);
+}
+
+
+$id = $_POST['id_soal'];
+if (isset($_POST['del'])) {
+if (hapus($id) > 0) {
+	echo "<script>
+			alert('Soal berhasil dihapus');
+			document.location.href = '../detail-soal.php';
+		</script>";
+} else {
+		"<script>
+			alert('Soal gagal dihapus');
+			document.location.href = '../detail-soal.php#hapus';
+		</script>";
+	}
+}
